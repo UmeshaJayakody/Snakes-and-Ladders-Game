99745858bb34c697bc1b51b17bfb5ea5627feea9
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2025-01-23 17:17:27 +0530

    initial commit

---
+++ Script/script.js
@@ -0,0 +1,405 @@
+const html_code  = `<div class="cell" ><span id="cell-100"></span></div>
+                    <div class="cell"><span id="cell-99"></span></div>
+                    <div class="cell"><span id="cell-98"></span></div>
+                    <div class="cell"><span id="cell-97"></span></div>
+                    <div class="cell"><span id="cell-96"></span></div>
+                    <div class="cell"><span id="cell-95"></span></div>
+                    <div class="cell"><span id="cell-94"></span></div>
+                    <div class="cell"><span id="cell-93"></span></div>
+                    <div class="cell"><span id="cell-92"></span></div>
+                    <div class="cell"><span id="cell-91"></span></div>
+                    <div class="cell"><span id="cell-81"></span></div>
+                    <div class="cell"><span id="cell-82"></span></div>
+                    <div class="cell"><span id="cell-83"></span></div>
+                    <div class="cell"><span id="cell-84"></span></div>
+                    <div class="cell"><span id="cell-85"></span></div>
+                    <div class="cell"><span id="cell-86"></span></div>
+                    <div class="cell"><span id="cell-87"></span></div>
+                    <div class="cell"><span id="cell-88"></span></div>
+                    <div class="cell"><span id="cell-89"></span></div>
+                    <div class="cell"><span id="cell-90"></span></div>
+                    <div class="cell"><span id="cell-80"></span></div>
+                    <div class="cell"><span id="cell-79"></span></div>
+                    <div class="cell"><span id="cell-78"></span></div>
+                    <div class="cell"><span id="cell-77"></span></div>
+                    <div class="cell"><span id="cell-76"></span></div>
+                    <div class="cell"><span id="cell-75"></span></div>
+                    <div class="cell"><span id="cell-74"></span></div>
+                    <div class="cell"><span id="cell-73"></span></div>
+                    <div class="cell"><span id="cell-72"></span></div>
+                    <div class="cell"><span id="cell-71"></span></div>
+                    <div class="cell"><span id="cell-61"></span></div>
+                    <div class="cell"><span id="cell-62"></span></div>
+                    <div class="cell"><span id="cell-63"></span></div>
+                    <div class="cell"><span id="cell-64"></span></div>
+                    <div class="cell"><span id="cell-65"></span></div>
+                    <div class="cell"><span id="cell-66"></span></div>
+                    <div class="cell"><span id="cell-67"></span></div>
+                    <div class="cell"><span id="cell-68"></span></div>
+                    <div class="cell"><span id="cell-69"></span></div>
+                    <div class="cell"><span id="cell-70"></span></div>
+                    <div class="cell"><span id="cell-60"></span></div>
+                    <div class="cell"><span id="cell-59"></span></div>
+                    <div class="cell"><span id="cell-58"></span></div>
+                    <div class="cell"><span id="cell-57"></span></div>
+                    <div class="cell"><span id="cell-56"></span></div>
+                    <div class="cell"><span id="cell-55"></span></div>
+                    <div class="cell"><span id="cell-54"></span></div>
+                    <div class="cell"><span id="cell-53"></span></div>
+                    <div class="cell"><span id="cell-52"></span></div>
+                    <div class="cell"><span id="cell-51"></span></div>
+                    <div class="cell"><span id="cell-41"></span></div>
+                    <div class="cell"><span id="cell-42"></span></div>
+                    <div class="cell"><span id="cell-43"></span></div>
+                    <div class="cell"><span id="cell-44"></span></div>
+                    <div class="cell"><span id="cell-45"></span></div>
+                    <div class="cell"><span id="cell-46"></span></div>
+                    <div class="cell"><span id="cell-47"></span></div>
+                    <div class="cell"><span id="cell-48"></span></div>
+                    <div class="cell"><span id="cell-49"></span></div>
+                    <div class="cell"><span id="cell-50"></span></div>
+                    <div class="cell"><span id="cell-40"></span></div>
+                    <div class="cell"><span id="cell-39"></span></div>
+                    <div class="cell"><span id="cell-38"></span></div>
+                    <div class="cell"><span id="cell-37"></span></div>
+                    <div class="cell"><span id="cell-36"></span></div>
+                    <div class="cell"><span id="cell-35"></span></div>
+                    <div class="cell"><span id="cell-34"></span></div>
+                    <div class="cell"><span id="cell-33"></span></div>
+                    <div class="cell"><span id="cell-32"></span></div>
+                    <div class="cell"><span id="cell-31"></span></div>
+                    <div class="cell"><span id="cell-21"></span></div>
+                    <div class="cell"><span id="cell-22"></span></div>
+                    <div class="cell"><span id="cell-23"></span></div>
+                    <div class="cell"><span id="cell-24"></span></div>
+                    <div class="cell"><span id="cell-25"></span></div>
+                    <div class="cell"><span id="cell-26"></span></div>
+                    <div class="cell"><span id="cell-27"></span></div>
+                    <div class="cell"><span id="cell-28"></span></div>
+                    <div class="cell"><span id="cell-29"></span></div>
+                    <div class="cell"><span id="cell-30"></span></div>
+                    <div class="cell"><span id="cell-20"></span></div>
+                    <div class="cell"><span id="cell-19"></span></div>
+                    <div class="cell"><span id="cell-18"></span></div>
+                    <div class="cell"><span id="cell-17"></span></div>
+                    <div class="cell"><span id="cell-16"></span></div>
+                    <div class="cell"><span id="cell-15"></span></div>
+                    <div class="cell"><span id="cell-14"></span></div>
+                    <div class="cell"><span id="cell-13"></span></div>
+                    <div class="cell"><span id="cell-12"></span></div>
+                    <div class="cell"><span id="cell-11"></span></div>
+                    <div class="cell"><span id="cell-1"></span></div>
+                    <div class="cell"><span id="cell-2"></span></div>
+                    <div class="cell"><span id="cell-3"></span></div>
+                    <div class="cell"><span id="cell-4"></span></div>
+                    <div class="cell"><span id="cell-5"></span></div>
+                    <div class="cell"><span id="cell-6"></span></div>
+                    <div class="cell"><span id="cell-7"></span></div>
+                    <div class="cell"><span id="cell-8"></span></div>
+                    <div class="cell"><span id="cell-9"></span></div>
+                    <div class="cell"><span id="cell-10"></span></div>`;
+
+const ladders = [
+    [5, 23], [8, 26], [10, 49], [19, 38], [21, 61], [28, 53],
+    [35, 47], [36, 57], [43, 77], [47, 76], [54, 88], [62, 96],
+    [67, 86], [70, 91], [72, 93], [80, 99]
+];
+
+const snakes = [
+    [44, 22], [46, 15], [48, 9], [52, 11], [59, 18], [64, 24],
+    [68, 2], [69, 33], [83, 39], [87, 50], [92, 51], [94, 71],
+    [95, 37], [98, 15]
+];
+
+let purple_loc = 1;
+let green_loc = 1;
+let win = [];
+const players = ["purple", "green"];
+
+var user = document.querySelector('.massage_sender');
+var robot = document.querySelector('.info_loader');
+var restart = document.querySelector('.restart');
+restart.addEventListener('click', function() {
+    location.reload();
+});
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function locate(color, position) {
+    const element = document.getElementById(`cell-${position}`);
+    if (element) {
+        element.classList.add(`dot-${color}`);
+        setTimeout(() => element.classList.add("visible"), 0);
+    } else {
+        console.error(`Element with ID cell-${position} not found.`);
+    }
+}
+
+function removeAllClassesExcept(elementId, classToKeep) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.className = classToKeep ? classToKeep : '';
+    } else {
+        console.error(`Element with ID ${elementId} not found.`);
+    }
+}
+function remove(color, position) {
+    var element = document.getElementById(`cell-${position}`);
+    if (element) {
+        element.classList.remove('visible');
+        setTimeout(() => {
+            element.classList.remove(`dot-${color}`);
+        }, 500); 
+    } else {
+        console.error(`Element with ID cell-${position} not found.`);
+    }
+}
+async function move(move_count, color) {
+    
+    let loc = color === "purple" ? purple_loc : green_loc;
+
+    if (loc + move_count > 100) {
+        console.log(`${color} cannot move beyond 100.`);
+        if(color === "purple") robot.innerHTML = "Try again to match to 100.";
+        if(color === "green") robot.innerHTML = "Dont match my number.";
+        return;
+    }
+    if (color == "green" ) {        
+        if (purple_loc == green_loc) {
+            var element = document.getElementById(`cell-${loc}`);
+            element.classList.add('dot-purple-stay');
+            removeAllClassesExcept(`cell-${loc}`, 'dot-purple-stay');
+        }
+        else {
+            var element = document.getElementById(`cell-${loc}`);
+            element.classList.remove('dot-green-stay');
+        }
+    }
+    if (color == "purple" ) {        
+        if (purple_loc == green_loc) {
+            var element = document.getElementById(`cell-${loc}`);
+            element.classList.add('dot-green-stay');
+            removeAllClassesExcept(`cell-${loc}`, 'dot-green-stay');
+        }
+        else {
+            var element = document.getElementById(`cell-${loc}`);
+            element.classList.remove('dot-purple-stay');
+        }
+    }
+    for (let step = 0; step < move_count; step++) {
+        loc++;
+        let audio = new Audio('../Audio/move.mp3');
+        audio.volume = 0.25;
+        audio.play().catch(error => {
+            console.error('Error playing audio:', error);
+        });
+        locate(color, loc);
+        await sleep(1000);
+       
+        remove(color,loc);
+    }
+
+    if (color === "purple") purple_loc = loc;
+    if (color === "green") green_loc = loc;
+
+    for (const [start, end] of ladders) {
+        if (loc === start) loc = end;
+    }
+    for (const [start, end] of snakes) {
+        if (loc === start) loc = end;
+    }
+
+    if (color === "purple") purple_loc = loc;
+    if (color === "green") green_loc = loc;
+
+    if (loc === 100) {
+        if (!win.includes(color)) {
+            win.push(color);
+        }
+        if (win[0] == "purple") {
+            rollBtn.disabled = true;
+            robot.innerHTML = `congradulations,you wins!`;
+        }
+        else {
+            robot.innerHTML = 'robot win the game!';
+        }
+    }
+    fetch_board(purple_loc, green_loc);
+}
+
+async function autorun(color,random_value) {
+    console.log(`${color} rolled: ${random_value}`);
+    await move(random_value, color);
+}
+
+function fetch_board(purple_loc, green_loc) {
+    document.getElementById("game-board").innerHTML = html_code;
+
+    if (purple_loc === green_loc) {
+        const element = document.getElementById(`cell-${purple_loc}`);
+        element.classList.add("dot-purple-green");
+    } else {
+        const purpleElement = document.getElementById(`cell-${purple_loc}`);
+        purpleElement?.classList.add("dot-purple-stay");
+        const greenElement = document.getElementById(`cell-${green_loc}`);
+        greenElement?.classList.add("dot-green-stay");
+    }
+}
+
+fetch_board(purple_loc, green_loc);
+robot.innerHTML = "Welcome to the game";
+user.innerHTML = "...";
+async function waiting(time){
+    await sleep(time);
+}
+waiting(10000);
+robot.innerHTML = "Roll the dice to start the game";
+async function Gameon(player,random_value) {
+    if (win.length < 1) {
+        await autorun(player,random_value);
+        await sleep(2000); 
+        rollBtn_1.disabled = false;
+        rollBtn.disabled = true;
+        user.innerHTML = "Your turn";
+        robot.innerHTML = "...";
+        randomDice_1();
+        }
+    else {
+        rollBtn.disabled = true;
+        if (win[0] == "purple") {
+            robot.innerHTML = `congradulations,you wins!`;
+        }
+        else {
+            robot.innerHTML = 'robot win the game!';
+        }
+    }
+}
+
+async function Gameon_1(player,random_value) {
+    if (win.length < 1) {
+        await autorun(player,random_value);
+        await sleep(2000); 
+        rollBtn.disabled = false;
+        rollBtn_1.disabled = true;
+        robot.innerHTML = "Your turn";
+        user.innerHTML = "...";
+        }
+    else {
+        rollBtn.disabled = true;
+        if (win[0] == "purple") {
+            robot.innerHTML = `congradulations,you wins!`;
+        }
+        else {
+            robot.innerHTML = 'robot win the game!';
+        }
+}
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+const dice = document.querySelector('.dice');
+const rollBtn = document.querySelector('.roll');
+
+const dice_1 = document.querySelector('.dice_1');
+const rollBtn_1 = document.querySelector('.roll_1');
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        if (rollBtn) {
+            rollBtn.click();
+        }
+    }
+});
+function randomDice() {
+    rollBtn_1.disabled = true;
+    const random = Math.floor(Math.random() * 6) + 1; 
+    robot.innerHTML = 'Well done !';
+    rollDice(random);
+}
+
+function rollDice(random) {
+    dice.style.animation = 'rolling 3s';
+    setTimeout(function () {
+        switch (random) {
+            case 1:
+                dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+            case 6:
+                dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+            case 2:
+                dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                break;
+            case 5:
+                dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+            case 3:
+                dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                break;
+            case 4:
+                dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                break;
+            default:
+                console.error('Invalid dice value');
+                break;
+        }
+        
+        robot.innerHTML = 'you moved ' + random + ' steps';
+        runGame(random);
+        dice.style.animation = 'none';
+    }, 3050); 
+}
+
+async function runGame(random) {
+    await sleep(1000);
+    Gameon("purple",random);
+}
+
+rollBtn.addEventListener('click', randomDice);
+
+async function autorunGame(random_1) {
+    await sleep(1000);
+    Gameon_1("green",random_1);
+}
+
+function randomDice_1() {
+    const random_1 = Math.floor(Math.random() * 6) + 1; 
+    rollDice_1(random_1);
+}
+
+function rollDice_1(random_1) {
+    dice_1.style.animation = 'rolling_1 3s';
+    setTimeout(function () {
+        switch (random_1) {
+            case 1:
+                dice_1.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+            case 6:
+                dice_1.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+            case 2:
+                dice_1.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                break;
+            case 5:
+                dice_1.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+            case 3:
+                dice_1.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                break;
+            case 4:
+                dice_1.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                break;
+            default:
+                console.error('Invalid dice value');
+                break;
+        }
+        
+        user.innerHTML = 'you moved ' + random_1 + ' steps';
+        autorunGame(random_1);
+        dice_1.style.animation = 'none';
+    }, 3050); 
+}
